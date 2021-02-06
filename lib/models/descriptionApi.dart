0e50e451e833e1f1a70faba58b9543414cbdb720
Felipe Ribeiro <felipe.engcontrole@gmail.com>
2021-02-06 01:34:05 -0300

    Fix: Description API update

--- lib/models/descriptionApi.dart
+++ lib/models/descriptionApi.dart
@@ -1,12 +1,16 @@
 class DescriptionAPI {
   Language language;
   List<FormDescriptions> formDescriptions;
+  List<FlavorTextEntries> flavorTextEntries;
   String name;
+  Version version;
 
   DescriptionAPI({
     this.formDescriptions,
     this.language,
     this.name,
+    this.flavorTextEntries,
+    this.version,
   });
 
   DescriptionAPI.fromJson(Map<String, dynamic> json) {
@@ -14,6 +18,12 @@ class DescriptionAPI {
       formDescriptions = new List<FormDescriptions>();
       json['form_descriptions'].forEach((v) {
         formDescriptions.add(new FormDescriptions.fromJson(v));
+      });
+    }
+    if (json['flavor_text_entries'] != null) {
+      flavorTextEntries = new List<FlavorTextEntries>();
+      json['flavor_text_entries'].forEach((v) {
+        flavorTextEntries.add(new FlavorTextEntries.fromJson(v));
       });
     }
     name = json['name'];
@@ -25,7 +35,59 @@ class DescriptionAPI {
       data['form_descriptions'] =
           this.formDescriptions.map((v) => v.toJson()).toList();
     }
+    if (this.flavorTextEntries != null) {
+      data['flavor_text_entries'] =
+          this.flavorTextEntries.map((v) => v.toJson()).toList();
+    }
     data['name'] = this.name;
+    return data;
+  }
+}
+
+class FlavorTextEntries {
+  String flavorText;
+  Language language;
+  Version version;
+
+  FlavorTextEntries({this.flavorText, this.language, this.version});
+
+  FlavorTextEntries.fromJson(Map<String, dynamic> json) {
+    flavorText = json['flavor_text'];
+    language = json['language'] != null
+        ? new Language.fromJson(json['language'])
+        : null;
+    version =
+        json['version'] != null ? new Version.fromJson(json['version']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['flavor_text'] = this.flavorText;
+    if (this.language != null) {
+      data['language'] = this.language.toJson();
+    }
+    if (this.version != null) {
+      data['version'] = this.version.toJson();
+    }
+    return data;
+  }
+}
+
+class Version {
+  String name;
+  String url;
+
+  Version({this.name, this.url});
+
+  Version.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    url = json['url'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['name'] = this.name;
+    data['url'] = this.url;
     return data;
   }
 }
