7d9f2bc06f74cc74fbbede8d59a80d3ada467cde
Felipe Ribeiro <felipe.engcontrole@gmail.com>
2021-02-06 01:04:26 -0300

    Fix: loadPokeAPIDescription logic fixed

--- lib/stores/Descriptions/description_store.dart
+++ lib/stores/Descriptions/description_store.dart
@@ -1,6 +1,7 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:mobx/mobx.dart';
-import 'package:pokedex_flutter_mobx/constants/api_consts.dart';
 import 'package:pokedex_flutter_mobx/constants/app_consts.dart';
 import 'package:pokedex_flutter_mobx/models/descriptionApi.dart';
 part 'description_store.g.dart';
@@ -10,41 +11,26 @@ class DescriptionStore = _DescriptionStoreBase with _$DescriptionStore;
 abstract class _DescriptionStoreBase with Store {
   Dio dio = Dio();
 
-  List<DescriptionAPI> _apiDescription;
+  DescriptionAPI _apiDescription;
 
   @action
   fetchPokeAPIDescription(String name) {
     _apiDescription = null;
-    loadPokeAPIDescription(name).then((value) {
-      _apiDescription = value;
-    });
+    loadPokeAPIDescription(name).then((value) => _apiDescription = value);
   }
 
-  Future<List<DescriptionAPI>> loadPokeAPIDescription(String name) async {
+  Future<DescriptionAPI> loadPokeAPIDescription(String name) async {
     name = name.toLowerCase();
     List<String> separa = name.split(' ');
     if (separa.length == 2 && (separa[0] == 'Mega')) {
       name = separa[1];
     }
-
     final response = await dio.get(PokeDescriptions().getDescription(name));
-    final list = response.data as List;
-
-    List<DescriptionAPI> _descriptions;
-    _descriptions = [];
-    try {
-      // if (response.statusCode == 200) print(response.statusCode.toString());
-      for (var poke in list) {
-        final pokemon = DescriptionAPI.fromJson(poke);
-        _descriptions.add(pokemon);
-      }
-      print('Sucesso! Adicionou na lista!');
-      print('Foram adicionados ${_descriptions.length} pokémon na lista!');
-    } catch (e) {
-      print('Response API error');
-      return null;
+    if (response.statusCode == 200) {
+      final rest = jsonDecode(response.data);
+      final desc = DescriptionAPI.fromJson(rest);
+      print('Sucesso! Retornou!');
+      return desc;
     }
-    print('Sucesso! Retornou!');
-    return _descriptions;
   }
 }
